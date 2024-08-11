0f8acc1a3be2ef7327f82cfcd4efcd395f1a7b4b
Sandara Wijethunga <140485221+Sandara-Git@users.noreply.github.com>
2024-08-11 19:10:07 +1000

    config.py

--- app/config.py
+++ app/config.py
@@ -1,6 +1,6 @@
 import os
 
 class Settings:
-    DATABASE_URL: str = os.getenv("postgresql://s224740132_sit772_part1_user:9mdgsfRF4HkWd24MUOs8MMcDbGIL1dQZ@dpg-cqs2hu8gph6c73a6n5rg-a.oregon-postgres.render.com/s224740132_sit772_part1")
+    DATABASE_URL: str = os.getenv("DATABASE_URL")
 
 settings = Settings()
